ce11ef5b08dc6d3bebbbeb52cded7e091e632396
Chris <chrisagilbert.cg@gmail.com>
2023-03-23 11:20:12 +0000

    Created FunctionMixin.

--- mixins.py
+++ mixins.py
@@ -1,12 +1,23 @@
-class Focusable:
-	"""
-	An inheritable abtract class which allows a window object to take focus
-	"""
-	pass
+class FunctionMixin:
 
-class Scrollable:
-	"""
-	An inheritable abstract class which allows a window object to scroll
-	Objects should override the .draw() method
-	"""
-	pass
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.FUNCTIONS = {}
+		self.functions = lambda key_press : FUNCTIONS.get(chr(key_press))
+
+	def add_function(self, key, callback):
+		"""
+		Saves a key-value pair to self.FUNCTIONS in the form of:
+			{key:callback}
+		Where 'key_press' is a chr instance, and 'callback' references
+			the function called when the key is pressed.
+		"""
+		new_mapping = {key : callback}
+		self.FUNCTIONS.update(new_mapping)
+
+	def add_function_dict(self, function_dict):
+		"""
+		Same as self.add_function, but takes a dict object
+			as an argument.
+		"""
+		self.FUNCTIONS.update(function_dict)
