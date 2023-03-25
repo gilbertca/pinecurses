76b54f2a1d5a34a2f43dcba33610467b8a7ae463
Chris <chrisagilbert.cg@gmail.com>
2023-03-25 10:38:09 +0000

    Made changes.

--- base_item.py
+++ base_item.py
@@ -13,3 +13,22 @@ def BaseItem(AbstractBaseItem, FunctionsMixin):
 		self.text = kwargs.get('text') # Item text to be displayed
 		# Additional attributes the item displays:
 		self.curses_attributes = kwargs.('curses_attributes')
+
+	def get_display_string(self, available_width):
+		"""
+		This function determines truncation and other effects
+			which may happen to self.text.
+		It takes into account the provided width, and chops
+			the display string following a determined pattern.
+		For now: only takes a display width, and then truncates
+			the self.text string using two '.' characters.
+		WE HAVE TO THINK ABOUT VERTICAL WIDTH AND HORIZONTAL WIDTH
+		"""
+		split_list = self.text
+		return display_string
+
+	def calculate_text_height(self):
+		"""
+		Returns length of self.text, as self.text is a list of display strings.
+		"""
+		return len(self.text)
