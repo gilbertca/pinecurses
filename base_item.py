927afb3aecb9d961816df3697dcef705de435ef4
Chris <chrisagilbert.cg@gmail.com>
2023-03-25 10:52:34 +0000

    Fix.

--- base_item.py
+++ base_item.py
@@ -1,7 +1,7 @@
 from abstract_base_item import AbstractBaseItem
 from mixins import FunctionsMixin
 
-def BaseItem(AbstractBaseItem, FunctionsMixin):
+class BaseItem(AbstractBaseItem, FunctionsMixin):
 	"""
 	Simplest Item which can be created and displayed.
 	Inherits it's attributes from AbstractBaseItem
