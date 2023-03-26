1eea5002b74bcbdff79de5492bb93223ef9dec63
Chris <chrisagilbert.cg@gmail.com>
2023-03-26 18:31:40 +0100

    Decided collecting attributes should be the responsibility of each AbstractBase class.

--- abstract_base_item.py
+++ abstract_base_item.py
@@ -4,6 +4,12 @@ class AbstractBaseItem:
 	Cannot be instantiated;
 		use BaseItem instead.
 	"""
-
-	def __init__(self, *args, **kwargs):
-		pass
+	def __init__(self, file_name=None, *args, **kwargs):
+		if file_name: # If file name is given:
+			self.ATR = parse_json(file_name)
+		elif kwargs: # If dictionary is given as K/V pairs:
+			self.ATR = kwargs
+		else: # If no attributes are present:
+			raise ValueError("Attributes are required to create a view.")
+		# self.atr is shorthand to access self.ATR
+		self.atr = lambda key : self.ATR.get(key)
