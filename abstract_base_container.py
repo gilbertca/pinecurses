6de5f6d5ef3c70e3414968c3b097b2b1d1cac2e6
Chris <chrisagilbert.cg@gmail.com>
2023-04-01 21:55:37 +0100

    Fix.

--- abstract_base_container.py
+++ abstract_base_container.py
@@ -3,5 +3,5 @@ class AbstractContainer(list):
 	The highest abstraction of a Container.
 	To be inherited by BaseContainer.
 	"""
-	def __init__(self, item_list=None *args, **kwargs):
+	def __init__(self, item_list=None, *args, **kwargs):
 		if item_list: super().__init__(item_list, *args, **kwargs)
