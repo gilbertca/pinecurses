2576c285e3146750cfb64b6f95d0eeb59d04ab32
Chris <chrisagilbert.cg@gmail.com>
2023-04-08 08:02:18 +0100

    Made changes to base_container.

--- base_container.py
+++ base_container.py
@@ -6,3 +6,8 @@ class Container(AbstractContainer):
 	"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+
+	def assign_item(self, item, start_index, height):
+		# Assign n spaces for the item's height:
+		for n in range(start_index, height):
+			self[n] = item
