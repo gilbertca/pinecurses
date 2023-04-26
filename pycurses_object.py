7f7243fe2b4e9924b60636d429eca919e7d3e324
Chris <chrisagilbert.cg@gmail.com>
2023-04-26 18:00:25 +0100

    Added ATTRIBUTES, attributes, fixed super().__init__.

--- pycurses_object.py
+++ pycurses_object.py
@@ -3,9 +3,14 @@ class PycursesObject(dict):
 	The base functionality which all Pycurses objects inherit.
 	"""
 	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
+		TODO: UNPACK A DICT INTO super().__init... for creation at beginning
+		super().__init__()
 		self.FUNCTIONS = {}
+		# Shortcut for FUNCTIONS:
 		self.functions = lambda key_press : self.FUNCTIONS.get(chr(key_press))
+		self.ATTRIBUTES = {}
+		# Shortcut for ATTRIBUTES:
+		self.attributes = lambda name : self.ATTRIBUTES.
 
 	def add_function(self, key, callback):
 		"""
