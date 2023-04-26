105e5b0fed5524b234e67b552df031a00e91de6a
Chris <chrisagilbert.cg@gmail.com>
2023-04-26 19:01:00 +0100

    Removing more unnecessary files.

--- .textview.py
+++
@@ -1,9 +0,0 @@
-from baseview import AbstractBaseView
-
-class TextView(AbstractBaseView):
-
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-
-	def draw_window(self, *args, **kwargs):
-		pass
