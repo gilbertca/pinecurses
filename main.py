8146221b050376aeb872e340965b6232513210d6
Chris <chrisagilbert.cg@gmail.com>
2023-04-26 07:38:08 +0100

    Added reference to Controller into main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import curses
+from base_controller import BaseController
 
 def main(stdscr):
 	"""
 	The main method to run and test this suite.
 	"""
-	pass
+	# Create the controller:
+	controller = Controller(stdscr)
 
 if __name__ == "__main__":
 	curses.wrapper(main)
