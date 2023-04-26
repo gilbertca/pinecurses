3f018222d223259cc8f6f55d1b244a36a0b904fc
Chris <chrisagilbert.cg@gmail.com>
2023-04-26 07:48:04 +0100

    Created the PycursesObject. This is the base object for all BaseControllers, BaseViews, and BaseItems. All of the objects inherit from the 'dict' type. Within each dict are the children of each class. For example, A Controller might appear as: {'view_name' : view_instance}, a View might appear as {'item_name' : item_instance}, and an Item might appear as {0 : "First printable string.", 1 "Second printable string."}

---
+++ pycurses_object.py
@@ -0,0 +1,25 @@
+class PycursesObject(dict):
+	"""
+	The base functionality which all Pycurses objects inherit.
+	"""
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.FUNCTIONS = {}
+		self.functions = lambda key_press : self.FUNCTIONS.get(chr(key_press))
+
+	def add_function(self, key, callback):
+		"""
+		Saves a key-value pair to self.FUNCTIONS in the form of:
+			{key:callback}
+		Where 'key_press' is a chr instance, and 'callback' references
+			the function called when the key is pressed.
+		"""
+		new_mapping = {key : callback}
+		self.FUNCTIONS.update(new_mapping)
+
+	def add_function_dict(self, function_dict):
+		"""
+		Same as self.add_function, but takes a dict object
+			as an argument.
+		"""
+		self.FUNCTIONS.update(function_dict)""
