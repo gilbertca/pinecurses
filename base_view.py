04b308461084c5a115836a94111af176ddd956da
Chris <chrisagilbert.cg@gmail.com>
2023-04-27 21:30:54 +0100

    Fixed silly issue whereby I did not define an init function to begin with.

--- base_view.py
+++ base_view.py
@@ -8,5 +8,6 @@ class BaseView(PycursesObject):
 	"""
 	The base View class which controls all items of a Pycurses program.
 	"""
-	super().__init__(self, parent_controller_instance, *args, **kwargs)
-	self.controller = parent_controller_instance
+	def __init__(self, parent_controller_instance, *args, **kwargs):
+		super().__init__(self, *args, **kwargs)
+		self.controller = parent_controller_instance
