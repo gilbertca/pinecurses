30185b4554b4152f08519ef0945996c7efd4f04e
Chris <chrisagilbert.cg@gmail.com>
2023-04-27 23:33:27 +0100

    Added BaseItem import statement.

--- base_view.py
+++ base_view.py
@@ -1,6 +1,6 @@
-import curses
 import math
 from pycurses_object import PycursesObject
+from base_item import BaseItem
 from utils import log
 
 
@@ -10,4 +10,5 @@ class BaseView(PycursesObject):
 	"""
 	def __init__(self, parent_controller_instance, *args, **kwargs):
 		super().__init__(self, *args, **kwargs)
+		# Assign parent:
 		self.controller = parent_controller_instance
