31c39dc90cb6268f34ffcaec3658f7ec87db032e
Chris <chrisagilbert.cg@gmail.com>
2023-04-29 12:04:16 +0100

    Added BaseItem.get_display_string_iterable(..). This function currently just checks width before returning a string. The Item should run a hypothetical 'BaseItem.pack(..)' to pack all of the attributes into nice unpackable parameters for curses functions. It should also know where the view has put it on the screen, and keep track of this with movements.

--- base_item.py
+++ base_item.py
@@ -9,3 +9,31 @@ class BaseItem(PycursesObject):
 	def __init__(self, parent_view_instance, *args, **kwargs):
 		super().__init__(self, *args, **kwargs)
 		self.view = parent_view_instance
+
+	def get_display_string_iterable(self):
+		"""
+		Returns and formats self.attributes('display_string'),
+			i.e. the text which is displayed by any view.
+		Formatting is based on:
+			1. Writable width
+			2. Controller specific tests
+		This code currently just returns a string within the width of the View's window.
+		"""
+		writable_width = self.view.get_writable_width()
+		display_string = self.attributes('display_string')
+		display_string_iter = display_string.split('\n')
+		truncation_character = self.attributes('truncation_character')
+		return_iterable = []
+		for iter_string in display_string_iter:
+			# If whole string fits on a line:
+			if len(iter_string) <= writable_width:
+				return_iterable.append(iter_string)
+			# Otherwise, if string is longer than width,
+			#	And a truncation character is present:
+			elif truncation_character:
+				return_iterable.append((iter_string[:writable_width-1] + truncation_character))
+			# Otherwise just return the string at length.
+			else:
+				return_iterable.append(iter_string[:writable_width])
+		# And return the list of strings:
+		return return_iterable
