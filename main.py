75ba425a319b638dc4d20e368ade07127056f437
Chris <chrisagilbert.cg@gmail.com>
2023-06-20 20:36:37 +0100

    Saving changes to current branch.

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import curses
 from pycurses_program import PycursesProgram
-from base_controller import BaseController
-from base_view import BaseView
-from base_item import BaseItem
+#from base_controller import BaseController
+#from base_view import BaseView
+#from base_item import BaseItem
 
 class DevelopmentProgram(PycursesProgram):
 
 	def __init__(self, json_directory, *args, **kwargs):
-		super().__init__(self, *args, **kwargs)
+		super().__init__(json_directory, *args, **kwargs)
+		"""
 		self.class_namespace.update({
 			'controller' : BaseController,
 			'view' : BaseView,
 			'item' : BaseItem
-		})
-		self.initialize(json_directory)
+		})"""
+		self.json_directory = json_directory
 
 def main(stdscr):
 	"""
 	The main method to run and test the suite.
 	"""
 	program = DevelopmentProgram('./json')
-	print(program.object_dict)
 	return program.begin()
 
 
--- pycurses_program.py
+++ pycurses_program.py
@@ -1,21 +1,28 @@
-from utils import parse_json_folder
+#from utils import parse_json_folder
 
 class PycursesProgram():
 	
-	def __init__(self, *args, **kwargs):
+	def __init__(self, json_directory, *args, **kwargs):
 		self.class_namespace = {}
+		self.json_directory = json_directory
 
-	def initialize(self, json_directory):
+	def load_objects(self):
 		"""
-		Must be run by the child PycursesProgram class,
-			due to self.class_namespace not containing anything during
-			self.__init__(..).
+		Reads through
 		"""
-		self.object_dict = parse_json_folder(self.class_namespace, json_directory)
+		self.object_dict = parse_json_folder(self.class_namespace, self.json_directory)
 
 	def begin(self):
 		"""
-		Passes control of the program to the Controller.
+		The main method of a PycursesProgram, which must be called as:
+		return *Program.begin()
+		This will read all JSON files and create the corresponding PycursesObjects,
+		Initialize the proper relationships between classes,
+		Pass control to a Controller and return its interact method.
 		"""
-		controller = self.object_dict.get('controller')[0]
-		return controller.begin(self.object_dict)
+		# Read all json, and load into a key-name dictionary:
+		pycurses_object_dict = self.load_objects()
+		# Once all objects are created, they must be initialized,
+		# 	e.g. screen position calculations are run after __init__.
+		
+		#return controller.begin(self.object_dict)
