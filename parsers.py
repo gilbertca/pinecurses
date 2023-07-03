8942dac1acf7496791873c192d21adc428f6d731
Chris <chrisagilbert.cg@gmail.com>
2023-07-03 19:49:27 +0100

    Finishing up JsonParser.parse(..). Next step is finishing the _create_objects_from_namespace(..) function, and potentially giving it a better name.

--- parsers.py
+++ parsers.py
@@ -15,22 +15,31 @@ class JsonParser(Parser):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 	
-	def parse(self):
+	def parse(self, class_namespace):
 		"""
 		parse returns a dictionary containing name:PycursesObjects.
 		JsonParser uses json.load(..) to read from .json files.
 		"""
-		pycurses_objects_dict = self.traverse()
+		pycurses_objects_dict = {}
+		directory_list = os.listdir(self.base_directory)
+		# NOTE: Directory name must match the name associated
+		#	with the corresponding class!
+		for current_directory_name in directory_list:
+			class_reference = class_namespace.get(current_directory_name)
+			filenames_list = os.listdir(f"{self.base_directory}/{current_directory_name}")
+			pycurses_objects_list = self._create_objects_from_namespace(
+				ClassReference, filenames_list, current_directory_name
+			)
+			pycurses_objects_dict.update({current_directory_name:pycurses_objects_list})
 		return pycurses_objects_dict
 
-	def _create_objects_from_namespace(self, ClassReference, file_list, current_directory):
+	def _create_objects_from_namespace(self, ClassReference, filenames_list, current_directory_name):
 		"""
 		Helper method which returns a list of objects created from a given ClassReference.
 		The attributes for each created object are read from the file_list
-			in the given current_directory.
+			in the given current_directory_name.
 		"""
 		pass
-
 
 	def parse_json(self, file_name):
 		"""
