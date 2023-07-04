f2bbab9ac66e6a75978110828cbabf9f0ab09cdd
Chris <chrisagilbert.cg@gmail.com>
2023-07-04 08:52:40 +0100

    All required functionality for a JsonParser sHoUlD be contained within the class now.

--- parsers.py
+++ parsers.py
@@ -6,19 +6,9 @@ class Parser:
 	def __init__(self, base_directory, *args, **kwargs):
 		self.base_directory = base_directory
 
-	def parse(self):
-		raise Exception("This method must be overloaded by a child class.")
-
-
-class JsonParser(Parser):
-	
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-	
 	def parse(self, class_namespace):
 		"""
 		parse returns a dictionary containing name:PycursesObjects.
-		JsonParser uses json.load(..) to read from .json files.
 		"""
 		pycurses_objects_dict = {}
 		directory_list = os.listdir(self.base_directory)
@@ -41,14 +31,34 @@ class JsonParser(Parser):
 		"""
 		pycurses_objects_list = []
 		for filename in filenames_list:
-			class_attributes = parse_json(f"{self.base_directory}/{current_directory_name}/{filename}")
+			class_attributes = parse_file(f"{self.base_directory}/{current_directory_name}/{filename}")
 			PycursesClass = ClassReference(**class_attributes)
 			pycurses_objects_list.append(PycursesClass)
 		return pycurses_objects_list
 
+	def parse_file(self, *args, **kwargs):
+		"""
+		This function is to be overloaded by a child class,
+			and is to return the contents of a file in the form
+			of a dictionary containing name:attribute pairs.
+		"""
+		raise Exception("This method must be overloaded by a child class.")
+
+
+class JsonParser(Parser):
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+	
+	def parse_file(self, file_name):
+		"""
+		Overloaded method which returns the value from parse_json.
+		"""
+		return parse_json(file_name)
+
 	def parse_json(self, file_name):
 		"""
-		Simple utility to read from a JSON file returns a dict object
+		Simple utility to read from a JSON file, returns a dict object.
 		"""
 		with open(file_name) as json_file:
 			data = json.load(json_file)
