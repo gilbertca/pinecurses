dc3e99bcfa42aec56e2cae6a9b70babe1c26f9bb
gilbertca <chrisagilbert.cg@gmail.com>
2023-10-01 01:12:53 -0400

    Changes to @log to increase readability.

--- logger.py
+++ logger.py
@@ -1,19 +1,24 @@
 import logging
 from datetime import datetime
+
+
 # Create a logger for this module
 logger = logging.getLogger(__name__)
 # Set the logging level
 logger.setLevel(logging.DEBUG)
-def log_t(message):
-	with open('LOG.pycurse', 'a') as log_file:
-		log_file.write(message + '\n')
+
 def log(function):
 	"""
 	Decorator to log info about internal functions and handle errors.
 	"""
 	def log(*args, **kwargs):
 		# Log the function name, arguments, and file
-		logger.debug(f"{datetime.now()} Function: {function.__name__} Args: {args} Kwargs: {kwargs} File: {function.__globals__.get('__file__')}")
+		logger.debug(f"{datetime.now()} \
+				\nFunction: {function.__name__} \
+				\nArgs: {args} \
+				\nKwargs: {kwargs} \
+				\nFile: {function.__globals__.get('__file__')} \
+			   \n")
 		try:
 			return function(*args, **kwargs)
 		except Exception as e:
