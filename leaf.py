4f542bc2391789c7613add2801211ecfeb5ba8ee
gilbertca <chrisagilbert.cg@gmail.com>
2023-10-02 06:31:43 -0400

    Added another child of Leaf; added boilerplate to Leaf.draw

--- leaf.py
+++ leaf.py
@@ -16,6 +16,9 @@ class Leaf(BaseObject):
 		"""Requests the Leaf's parent window object, and draws itself to the window.
 		"""
 		branch_window = self.parent.window
+		drawing_instruction_list = self._get_drawing_instructions()
+		for instruction in drawing_instruction_list:
+			branch_window.addstr(instruction)
 
 	def _get_drawing_instructions(self):
 		"""Returns a list of instructions which are to be used by self.draw. This method must be overloaded by a child element.
@@ -39,6 +42,13 @@ class BarLeaf(Leaf):
 
 class ListLeaf(Leaf):
 	"""A ListLeaf is a Leaf which requests a list of strings, and displays each string in a vertical list. The height of each item is assumed to be 1, unless explicitly set.
+	"""
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+
+class TextboxLeaf(Leaf):
+	"""A TextboxLeaf is a Leaf which relies on a curses Textbox to gather user input, and to return that input to the User's programmed API.
 	"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
