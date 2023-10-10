ed93a47f5882778c0cf96fa6a3c3be2b694042e4
Christopher Gilbert <chrisagilbert.cg@gmail.com>
2023-10-10 09:54:51 -0400

    Removed a code from old schema.

--- trunk.py
+++ trunk.py
@@ -4,8 +4,9 @@ from mixins.cursormixins import SingleObjectCursor
 from logger import log
 
 class Trunk(SingleObjectCursor, BaseObject):
+	"""A Trunk is considered the Base of a Pinecurses program, and is typically created by a Pinecurses object. After creation, running `trunk_instance.begin(..)` will begin the program.
 	"""
-	"""
+	style_filename = "trunk.json"
 	def __init__(self, *args, **kwargs):
 		super().__init__(self, *args, **kwargs)
 		self.CURSES_COLOR_MAP = {
@@ -23,15 +24,12 @@ class Trunk(SingleObjectCursor, BaseObject):
 		self.FUNCTIONS.update({'x' : lambda:0})
 
 	@log
-	def begin(self, stdscr, **object_dict):
+	def begin(self, stdscr):
 		"""
 		The main loop of any pinecurses program. Once this function returns anything,
 			then the program will end.
 		"""
 		self.window = stdscr
-		self.initialize(**object_dict)
-		self.map_all_colors()
-		self.draw_all_views()
 		# Once self.interact(..) returns a value, program will end.
 		while True:
 			# Get the keypress from a child window:
@@ -39,78 +37,3 @@ class Trunk(SingleObjectCursor, BaseObject):
 			if self.interact(keypress) == 0:
 				return 0
 
-	@log
-	def color(self, view_name, color_name):
-		"""
-		A shortcut for accessing the nested structure of self.colors.
-		self.colors = {
-			"view_name" : {"color_name" : 1 <-- returns this},
-		}
-		"""
-		return self.colors.get(view_name).get(color_name)
-
-	@log
-	def initialize(self):
-		"""MAY NEED TO REMOVE THIS FUNCTION AS SCHEMA CHANGES.
-		"""
-		pass
-
-	@log
-	def draw_all_views(self):
-		"""
-		Draws all views within self's dictionary.
-		"""
-		for view_instance in self.children:
-			view_instance.draw_self()
-
-	@log
-	def map_colors(self, view_instance):
-		"""
-		Maps all colors within the Controller.
-		"""
-		view_name = view_instance.attributes('name')
-		self.colors.update({view_name : {}})
-		color_attributes = view_instance.get_color_attributes()
-		# Iterate through color defining attributes:
-		for color_key in color_attributes:
-			color_value = color_attributes.get(color_key)
-			pair_number = self._next_color_pair()
-			# color_attributes will contain default values automatically!
-			# TODO: NEED 3 CASES: 0 VALUE, STRING VALUE, LIST VALUE
-			if color_value == 0: # Map default black on white case:
-				# Remember: No need to initialize pair number 0
-				#due to curses' hardcoded values.
-				self.colors.get(view_name).update({color_key : 0})
-			elif isinstance(color_value, str): # Map text on default background:
-				# Request the Controller's default background:
-				colors = [self.CURSES_COLOR_MAP.get(color_value), self.DEFAULT_BACKGROUND_COLOR]
-				# Update self's reference from 'text_color' to a curses pair number:
-				self.colors.get(view_name).update({color_key : pair_number})
-				# Finally initialize the pair within curses:
-				curses.init_pair(pair_number, *colors)
-			elif isinstance(color_value, list): # Map text and background:
-				# Begin by taking the names of the colors, and getting
-				#their curses counterpart integers, and pack into a list:
-				colors = [self.CURSES_COLOR_MAP.get(color) for color in color_value]
-				# Update the reference in self.colors:
-				self.colors.get(view_name).update({color_key : pair_number})
-				# Initialize the pair within curses:
-				curses.init_pair(pair_number, *colors)
-
-	@log
-	def _next_color_pair(self):
-		"""
-		Returns an integer equal to the next init-able curses color pair.
-		"""
-		count = 1
-		# Iterate and count through all views:
-		for view_instance in self.children:
-			view_name = view_instance.name
-			color_dict = self.colors.get(view_name)
-			# Iterate through the View's color dictionary:
-			for color_key in color_dict:
-				# Remember: color_integer is a curses color pair.
-				color_integer = color_dict.get(color_key)
-				if color_integer > 0:
-					count += 1
-		return count
