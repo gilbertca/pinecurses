b2d647b01214623648df853565ee853f7e1fc167
Christopher Gilbert <chrisagilbert.cg@gmail.com>
2023-10-21 07:30:31 -0400

    Program runs.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pinecurses import Pinecurses
+from example import ExampleTrunk
 
 def main():
 	"""
 	The main method to run and test the suite.
 	"""
-	program = Pinecurses('./styles')
+	program = Pinecurses('./styles', BaseClassReference=ExampleTrunk)
 	program.begin()
 
 
