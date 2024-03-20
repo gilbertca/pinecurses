4d16593586b7d55ed16e6ba3234df2905debccb3
gilbertca <chrisagilbert.cg@gmail.com>
2024-03-20 07:20:28 -0400

    Removed unused 'parse' functions from parsers.parser

--- parsers/parser.py
+++ parsers/parser.py
@@ -6,22 +6,6 @@ class Parser:
 	def __init__(self, base_directory, *args, **kwargs):
 		self.base_directory = base_directory
 
-	def parse(self, class_namespace):
-		"""
-		parse returns a dictionary containing name:PycursesObjects.
-		"""
-		pinecurses_objects_dict = {}
-		directory_list = os.listdir(self.base_directory)
-		# NOTE: Directory name must match the name associated in Pinecurses.class_namespace
-		for current_directory_name in directory_list:
-			ClassReference = class_namespace.get(current_directory_name)
-			filenames_list = os.listdir(f"{self.base_directory}/{current_directory_name}")
-			pinecurses_objects_list = self._create_objects_from_namespace(
-				ClassReference, filenames_list, current_directory_name
-			)
-			pinecurses_objects_dict.update({current_directory_name:pinecurses_objects_list})
-		return pinecurses_objects_dict
-
 	def _create_objects_from_namespace(self, ClassReference, filenames_list, current_directory_name):
 		"""
 		Helper method which returns a list of objects created from a given ClassReference.
