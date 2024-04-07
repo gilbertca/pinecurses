a045f9008fd782afb0e546fa5f2a2637250ae0fe
gilbertca <chrisagilbert.cg@gmail.com>
2024-04-07 01:41:59 -0400

    Simplified and moved 'parsers'

--- parser.py
+++ parser.py
@@ -1,5 +1,19 @@
 import json
-from parsers.parser import Parser
+import os
+
+class Parser:
+	
+	def __init__(self, base_directory, *args, **kwargs):
+		self.base_directory = base_directory
+
+	def parse_file(self, *args, **kwargs):
+		"""
+		This function is to be overloaded by a child class,
+			and is to return the contents of a file in the form
+			of a dictionary containing name:attribute pairs.
+		"""
+		raise Exception("This method must be overloaded by a child class.")
+
 
 class JsonParser(Parser):
 	
