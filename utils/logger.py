06395b8d55b68e3afbe21873c7e53b1e7f1811f8
name <name@website.com>
2024-09-26 11:42:07 -0400

    Starting from scratch with new architecture.

--- utils/logger.py
+++ utils/logger.py
@@ -7,11 +7,21 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 def log(function):
-	"""
-	Decorator to log info about internal functions and handle errors.
+	"""**log** is a decorator which provides detailed information about the application at runtime.
+
+	It is intended to decorate all methods for all classes involved in the application. 
+	Successful usage of the log decorator will provide the developer with a detailed breakdown 
+	of all function calls which occurred during the program's life, and a detailed stackstrace
+	in the event of errors.
+
+	**log** is not responsible for *handling* any errors. All exceptions should be logged and re-raised.
+
+	Authors:
+		@gilbertca
+		@amin1029384756
 	"""
 	def log(*args, **kwargs):
-		# Log the function name, arguments, and file
+		# Log function name, args and kwargs, and originating functions filename. 
 		logger.debug(f"{datetime.now()}\
 				\nFunction: {function.__name__}\
 				\nArgs: {args}\
@@ -20,7 +30,7 @@ def log(function):
 		try:
 			return function(*args, **kwargs)
 		except Exception as e:
-			# Log the exception
+			# Log the exception and re-raise. We do not need exc_info, since our trace is detailed enough.
 			logger.critical(f"Critical error in function {function.__name__} due to {e}", exc_info=False)
 			raise
 
--- utils/parser.py
+++ utils/parser.py
@@ -1,36 +1,29 @@
 import json
-import os
+import tomllib
 
-class Parser:
-	
-	def __init__(self, base_directory, *args, **kwargs):
-		self.base_directory = base_directory
+def parse_json(file_name):
+	with open(file_name) as file:
+		return json.load(file)
 
-	def parse_file(self, *args, **kwargs):
-		"""
-		This function is to be overloaded by a child class,
-			and is to return the contents of a file in the form
-			of a dictionary containing name:attribute pairs.
-		"""
-		raise Exception("This method must be overloaded by a child class.")
+def parse_toml(file_name):
+	with open(file_name) as file:
+		return toml.load(file)
 
+def parse_file(file_name, file_type):
+	"""**parse_file** returns structured data from a file for Pinecurses.
 
-class JsonParser(Parser):
-	
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-	
-	def parse_file(self, file_name):
-		"""
-		Overloaded method which returns the value from parse_json.
-		"""
-		return self._parse_json(file_name)
-
-	def _parse_json(self, file_name):
-		"""
-		Simple utility to read from a JSON file, returns a dict object.
-		"""
-		with open(file_name) as json_file:
-			data = json.load(json_file)
-		return data
-
+	**parse_file** will match **file_type** to a deserialization function.
+	The file will be parsed by the function and the data will be returned
+	with Python's data types.
+	"""
+	deserializing_functions = {
+		'json': parse_json,
+		'toml': parse_toml,
+	}
+	deserializer = deserializing_function.get(file_type)
+	if deserializer is None: # Unsupported file format
+		if file_type.startswith('.'):
+			error_message = "Remove the '.' from file_type and try again."
+		else:
+			error_message = f"file_type {file_type} is not supported at this time."
+		raise ValueError(error_message)
