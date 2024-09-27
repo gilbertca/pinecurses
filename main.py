3457211d33096a98ed98b402ba772da8ef1f8792
name <name@website.com>
2024-09-27 05:46:51 -0400

    curses.panel must be tested, since the documentation is rather sparse. Code clears without errors at this time.

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ def main():
 	"""
 	The main method to run and test the suite.
 	"""
-	program = Pinecurses('./templates', 'json')
-	program.CLASS_REFERENCES.update({
-	})
+	template_dir_name = './templates'
+	template_file_type = 'json'
+	custom_template_classes = {}
+	custom_keybindings = {}
+	program = Pinecurses(
+			template_dir_name,
+			template_file_type,
+			template_classes=custom_template_classes,
+			keybindings = custom_keybindings,
+			)
 	program.begin()
 
 
