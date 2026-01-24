37d845f659da6c7c609263c18c7adc878f32f5bc
gilbertca <44333250+gilbertca@users.noreply.github.com>
2026-01-23 20:55:55 -0500

    Uncommented  so we can track errors

--- src/parsing.py
+++ src/parsing.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def parse_pinecurses_config(expanded_template):
     """Accepts an expanded template and pulls all Pinecurses tags from it."""
-    #soup = BeautifulSoup(expanded_template, "html.parser")
+    soup = BeautifulSoup(expanded_template, "html.parser")
     tags = {}
     # Extract important tags, convert to curses equivalents
     return tags
