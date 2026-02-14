2d4770ef4bffdbad2c64bc8599b87fc083691546
gilbertca <44333250+gilbertca@users.noreply.github.com>
2026-02-14 15:32:28 -0500

    Added interface to update context to templater

--- src/templating.py
+++ src/templating.py
@@ -3,19 +3,26 @@ into data structures parseable by the **parsing** module using **jinja2**."""
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 class PinecursesTemplater:
-    def __init__(self, template_directory):
+    def __init__(self, template_directory, context={}):
         self.environment = Environment(
             loader=FileSystemLoader(template_directory),
             autoescape=select_autoescape()
         )
+        self.context = context
 
     def expand_pinecurses_template(
             self,
             template_filename,
-            **context
         ):
         """Reads a Pinecurses template and expands dynamic content.
         This is the first step, parsing is the next step (see **parsing**)."""
         template = self.environment.get_template(template_filename)
-        return template.render(context)
+        return template.render(self.context)
 
+    def update_context(self, new_context={}):
+        """Updates the Templater's context. Behavior is `dict.update`."""
+        self.context.update(new_context)
+
+    def replace_context(self, new_context={}):
+        """Replaces the Templater's context. Behavior is an assignment op."""
+        self.context = new_context
